f4421d228c9236d6ae57a2fde754268411ebdccf
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2025-11-13 10:21:19 -0600

    drawplate work, offhanded now

---
+++ instrumentssbc/src/ItemTypes/Drawplate/DrawplateItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+
+namespace instrumentssbc.src.ItemTypes.Drawplate
+{
+    internal class DrawplateItem : Item
+    {
+        public DrawplateItem() 
+        {
+        
+        }
+
+        
+    }
+}
---
+++ instrumentssbc/src/ItemTypes/Drawplate/IOffhandDominantInteractionItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+
+namespace instrumentssbc.src.ItemTypes.Drawplate
+{
+    internal interface IOffhandDominantInteractionItem
+    {
+
+        //It is important to know that this will send the OffhandItem the Main Hand slot! And not the Offhand one like in the _actual_ calls.
+        //This returns false if it should steal the call, and true if it should let it keep going.
+        public abstract bool HasOffhandInteractionAvailable(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent);
+    }
+}
