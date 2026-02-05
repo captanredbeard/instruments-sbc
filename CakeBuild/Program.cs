3fc9939113a9727fc47fba11212c991799c3024c
captanredbeard <76228828+captanredbeard@users.noreply.github.com>
2026-02-05 11:50:31 -0600

    version bump and nylon guitar fix

--- CakeBuild/Program.cs
+++ CakeBuild/Program.cs
@@ -107,7 +107,7 @@ namespace CakeBuild
             {
                 context.CopyFile($"../{BuildContext.ProjectName}/modicon.png", $"../Releases/{context.Name}/modicon.png");
             }
-            context.Zip($"../Releases/{context.Name}", $"../Releases/{context.Name}_{context.Version}.zip");
+            context.Zip($"../Releases/{context.Name}", $"../Releases/{context.Name}_{context.Version}_aos.zip");
         }
     }
 
